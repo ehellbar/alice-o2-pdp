3459f939a734a0a6fc0ee7cb048f2773b844e2ac
Ernst Hellbar <ernst.hellbar@cern.ch>
2024-08-21 17:33:19 +0200

    adding script for generation of replay data

---
+++ rawTF2raw/generate_rawtf_indices.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# todo: replace all local variables with global ones form input args
+# source $ALICEO2PDP/rawTF2raw/generate_rawtf_indices.sh
+
+print_help() {
+  cat << EOF 
+  Script to extract consecutive timeframes from a list of rawtf files and create the corresponding raw data
+
+  Functions sourced by sourcing script without parameters:
+    - check_tfs_per_file
+      - print the average number of TFs from a small subset of rawtf files from the input file list
+    - sort_tfs
+      - sort the TFs from the input file list in continuous order and save the corresponding timeslice ids in the order they appear in the input file list
+      - output:
+        - sorted list with TF timing info
+        - file with timeslice indices to be used for raw data creation
+    - sort_tfs_check_input
+      - same as sort_tfs, but in addition make sure that number of inputs matches the desired number of inputs given by parameter 6
+
+  Parameters:
+    - param1: run mode
+      - 1: run creation of raw data from previously created input
+      - any other value sets the global variables defined by other input parameters and sources the functions
+    - param2: rawtf input file list
+    - param3: output directory
+    - param4: number of TFs to process
+    - param5: counter index of first TF to process 
+    - param6: number of Blocks to be expected per TF to select data for all included detectors
+          - used in sort_tfs_check_input
+          - if number of inputs is irrelevant, can be set to arbitrary value and sort_tfs can be used instead
+EOF
+
+  return
+}
+[[ $# == 0 ]] && print_help
+
+# input parameters
+runMode=$1
+rawtfFileList=$2
+outputDir=$3
+nTFs=$4
+firstTF=$5
+nBlocks=$6
+
+# runMode=0
+# rawtfFileList=rawtflist_LHC24ak_553146.txt
+# outputDir=$(date +"%Y-%m-%d")-pp-500kHz-replay-LHC24ak_553146_500tf
+# nTFs=500
+# firstTF=3500
+# nBlocks=15
+
+# output file names
+tfs_sorted=tfids_$(echo ${rawtfFileList} | sed 's/.txt//g' | awk -F 'rawtflist_' '{print $2}')_sorted.txt
+timeslices_sorted=timeslices_$(echo ${rawtfFileList} | sed 's/.txt//g' | awk -F 'rawtflist_' '{print $2}')_sorted.txt
+
+# export env variables
+## print prcoessing time info
+export DPL_REPORT_PROCESSING=1
+
+# sourced functions
+check_tfs_per_file() {
+  nFiles=10
+  nTFs=$(o2-raw-tf-reader-workflow --raw-only-det all --shm-segment-size 16000000000 --input-data $(cat rawtflist_LHC24ak_553146.txt | head -n ${nFiles} | sed -z 's/\n/,/g') -b --run | grep 'loops were sent' | awk -F' ' '{print $3}')
+  echo "${nTFs} TFs found in ${nFiles} files: $(echo $((nTFs * 10000 / nFiles)) | sed -e 's/....$/.&/;t' -e 's/.$/.0&/') TFs per file"
+}
+
+sort_tfs() {
+  time o2-raw-tf-reader-workflow --raw-only-det all --shm-segment-size 16000000000 --input-data ${rawtfFileList} -b --run | grep 'Done processing' | sed 's/,//g' | grep tf-reader | awk '{print $5,$6,$7,$9}' | sort -t ' ' -k 2 >${tfs_sorted}
+  firstLine=$(grep -nr tfCounter:${firstTF} ${tfs_sorted} | awk -F ':' '{print $1}')
+  tail -n +${firstLine} ${tfs_sorted} | head -n ${nTFs} | awk '{print $1}' | sort -V | sed -z -e 's/timeslice://g ;  s/\n/,/g ; s/,$//g' >${timeslices_sorted}
+}
+
+sort_tfs_check_input() {
+  time o2-raw-tf-reader-workflow --raw-only-det all --shm-segment-size 16000000000 --input-data ${rawtfFileList} -b --run | grep "Block:${nBlocks}" -A 6 | grep 'tf-reader.*Done processing' | sed 's/,//g' | awk '{print $5,$6,$7,$9}' | sort -t ' ' -k 2 >${tfs_sorted}
+  firstLine=$(grep -nr tfCounter:${firstTF} ${tfs_sorted} | awk -F ':' '{print $1}')
+  tail -n +${firstLine} ${tfs_sorted} | head -n ${nTFs} | awk '{print $1}' | sort -V | sed -z -e 's/timeslice://g ;  s/\n/,/g ; s/,$//g' >${timeslices_sorted}
+}
+
+# creation of raw data
+if [ "0${runMode}" -eq "01" ]; then
+  echo "LID=$(cat ${timeslices_sorted})" | tee ${outputDir}.log
+  LID=$(cat ${timeslices_sorted})
+  echo "o2-raw-tf-reader-workflow --raw-only-det all  --shm-segment-size 16000000000  --input-data ${rawtfFileList} --select-tf-ids " '$LID' " | o2-raw-data-dump-workflow --tof-input-uncompressed  --shm-segment-size 16000000000 --fatal-on-deadbeef --output-directory  ${outputDir} --dump-verbosity 1 --run | tee -a ${outputDir}.log" | tee -a ${outputDir}.log
+  o2-raw-tf-reader-workflow --raw-only-det all --shm-segment-size 16000000000 --input-data ${rawtfFileList} --select-tf-ids "$LID" | o2-raw-data-dump-workflow --tof-input-uncompressed --shm-segment-size 16000000000 --fatal-on-deadbeef --output-directory ${outputDir} --dump-verbosity 1 --run | tee -a ${outputDir}.log
+fi
